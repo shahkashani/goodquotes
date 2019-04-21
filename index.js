9be9bb492f1dd1962aca68c3f9a0975ba4323bd7
Shah Kashani <skashani@gmail.com>
2019-04-21 15:39:40 -0400

    Support for languages

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
+const lngDetector = new (require('languagedetect'))();
+
 const argv = require('yargs')
   .usage('Usage: $0 <command> [options]')
   .number('length')
   .required('tag')
   .default('length', 100)
-  .describe('tag', 'What to search for')
+  .describe('tag', 'Tag to search for')
+  .describe('language', 'Language to restrict quotes to (e.g. "french")')
   .describe('length', 'Maximum quote length').argv;
 
-const { tag, length } = argv;
+const { tag, length, language } = argv;
+
+const getLanguage = text => {
+  const [result] = lngDetector.detect(text, 1);
+  return result.length ? result[0] : null;
+};
 
 const getQuotes = async (tag, page = 1) => {
   const url = `https://www.goodreads.com/quotes/tag/${tag}?page=${page}`;
@@ -16,7 +24,7 @@ const getQuotes = async (tag, page = 1) => {
   const $ = cheerio.load(body);
   const quoteElements = $('.quoteText');
   return quoteElements
-    .map((i, quoteElement) => {
+    .map((_i, quoteElement) => {
       const text = $(quoteElement).text();
       const match = text.match(/\“(.*)\”/);
       return match ? match[1] : text;
@@ -24,18 +32,26 @@ const getQuotes = async (tag, page = 1) => {
     .get();
 };
 
-const getAllQuotes = async (tag, length) => {
+const getAllQuotes = async ({ tag, filter, onProgress }) => {
   let page = 1;
   let stop = false;
   while (!stop) {
     const quotes = await getQuotes(tag, page);
-    const shortQuotes = quotes.filter(q => q.length <= length);
+    const filteredQuotes = filter ? quotes.filter(filter) : quotes;
     stop = quotes.length === 0;
     page += 1;
-    if (shortQuotes.length) {
-      console.log(shortQuotes.join('\n'));
+    if (filteredQuotes.length) {
+      onProgress(filteredQuotes);
     }
   }
 };
 
-getAllQuotes(tag, length);
+getAllQuotes({
+  tag,
+  filter: q => q.length <= length && (!language || getLanguage(q) === language),
+  onProgress: quotes => {
+    if (quotes.length) {
+      console.log(quotes.join('\n'));
+    }
+  }
+});
